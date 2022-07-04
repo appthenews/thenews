1a12a27b13ab87c604f6afac6c612c11dd053039
vaux <zero.griffin@gmail.com>
2022-07-04 15:39:53 +0200

    Toggle bars

--- macOS/Middlebar.swift
+++ macOS/Middlebar.swift
@@ -2,13 +2,26 @@ import AppKit
 import Combine
 
 final class Middlebar: NSVisualEffectView {
+    var show: Bool {
+        didSet {
+            width?.constant = show ? 180 : 1
+            UserDefaults.standard.set(show, forKey: "middlebar")
+        }
+    }
+    
+    private weak var width: NSLayoutConstraint?
+    private var subs = Set<AnyCancellable>()
+    
     required init?(coder: NSCoder) { nil }
     init(session: Session) {
+        show = UserDefaults.standard.value(forKey: "middlebar") as? Bool ?? true
+        
         super.init(frame: .zero)
         state = .active
         material = .popover
         translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: 180).isActive = true
+        width = widthAnchor.constraint(equalToConstant: show ? 180 : 0)
+        width!.isActive = true
         
         let separator = Separator()
         addSubview(separator)
@@ -17,5 +30,12 @@ final class Middlebar: NSVisualEffectView {
         separator.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         separator.widthAnchor.constraint(equalToConstant: 1).isActive = true
         separator.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
+        session
+            .middlebar
+            .sink { [weak self] in
+                self?.show.toggle()
+            }
+            .store(in: &subs)
     }
 }
--- macOS/Sidebar.swift
+++ macOS/Sidebar.swift
@@ -2,13 +2,26 @@ import AppKit
 import Combine
 
 final class Sidebar: NSVisualEffectView {
+    var show: Bool {
+        didSet {
+            width?.constant = show ? 120 : 0
+            UserDefaults.standard.set(show, forKey: "sidebar")
+        }
+    }
+    
+    private weak var width: NSLayoutConstraint?
+    private var subs = Set<AnyCancellable>()
+    
     required init?(coder: NSCoder) { nil }
     init(session: Session) {
+        show = UserDefaults.standard.value(forKey: "sidebar") as? Bool ?? true
+        
         super.init(frame: .zero)
         state = .active
         material = .hudWindow
         translatesAutoresizingMaskIntoConstraints = false
-        widthAnchor.constraint(equalToConstant: 120).isActive = true
+        width = widthAnchor.constraint(equalToConstant: show ? 120 : 0)
+        width!.isActive = true
         
         let separator = Separator()
         addSubview(separator)
@@ -17,5 +30,12 @@ final class Sidebar: NSVisualEffectView {
         separator.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         separator.widthAnchor.constraint(equalToConstant: 1).isActive = true
         separator.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
+        session
+            .sidebar
+            .sink { [weak self] in
+                self?.show.toggle()
+            }
+            .store(in: &subs)
     }
 }
