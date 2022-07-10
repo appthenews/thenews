7f488647f47284d8c604e32d0045014da09753a8
vaux <zero.griffin@gmail.com>
2022-07-10 14:52:04 +0200

    Update text

--- macOS/List.Cell.swift
+++ macOS/List.Cell.swift
@@ -11,8 +11,8 @@ extension List {
                 
                 frame = info.rect
                 vibrant.frame.size.height = info.rect.height
-                label.attributedStringValue = info.string
-                label.frame.size.height = info.rect.height - 30
+                a.string = info.string
+                a.frame.size.height = info.rect.height - 30
             }
         }
         
@@ -24,6 +24,7 @@ extension List {
         
         private weak var label: Text!
         private weak var vibrant: Vibrant!
+        private weak var a: CATextLayer!
         
         required init?(coder: NSCoder) { nil }
         required init() {
@@ -33,17 +34,25 @@ extension List {
             vibrant.layer!.cornerRadius = 12
             vibrant.frame = .init(x: 10, y: 0, width: 260, height: 0)
             self.vibrant = vibrant
+
+            let a = CATextLayer()
+            a.frame = .init(x: 15, y: 15, width: 230, height: 0)
+            a.contentsScale = 2
+            a.isWrapped = true
+            self.a = a
+            
             
             let label = Text(vibrancy: true)
             label.translatesAutoresizingMaskIntoConstraints = true
             label.frame = .init(x: 25, y: 15, width: 230, height: 0)
-//            label.wantsLayer = true
-//            label.layer!.masksToBounds = false
+            label.wantsLayer = true
+            label.layer!.masksToBounds = false
             self.label = label
             
             super.init(frame: .zero)
             addSubview(vibrant)
             addSubview(label)
+            vibrant.layer!.addSublayer(a)
         }
         
         override func updateLayer() {
