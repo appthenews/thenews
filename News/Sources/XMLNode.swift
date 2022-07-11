b60561c192125fb25f6b55ca90ec66378ef1c2b7
vaux <zero.griffin@gmail.com>
2022-07-11 19:16:45 +0200

    Update extension

--- News/Sources/XMLNode.swift
+++ News/Sources/XMLNode.swift
@@ -13,7 +13,6 @@ extension XMLNode {
             let date = try? Date(pubDate, strategy: strategy)
         else { return nil }
         lol()
-        lol2()
         return (id: guid,
                 item: .init(feed: feed,
                             title: title,
@@ -33,12 +32,30 @@ extension XMLNode {
     private func lol() {
         
         let asd = """
-<s><p>Reuters revealed that Germany and Qatar have hit difficulties in talks over long-term liquefied natural gas (LNG) supply deals amid differences over [&#8230;]</p>\n<p>The post <a rel=\"nofollow\" href=\"https://www.reutersagency.com/en/reutersbest/article/reuters-reveals-germany-qatar-at-odds-over-terms-in-talks-on-lng-supply-deal/\">Reuters reveals Germany, Qatar at odds over terms in talks on LNG supply deal</a> appeared first on <a rel=\"nofollow\" href=\"https://www.reutersagency.com/en/\">Reuters News Agency</a>.</p>\n</s>
+<p>Reuters revealed that Germany and Qatar have hit difficulties in talks over long-term liquefied natural gas (LNG) supply deals amid differences over [&#8230;]</p>\n<p>The post <a rel=\"nofollow\" href=\"https://www.reutersagency.com/en/reutersbest/article/reuters-reveals-germany-qatar-at-odds-over-terms-in-talks-on-lng-supply-deal/\">Reuters reveals Germany, Qatar at odds over terms in talks on LNG supply deal</a> appeared first on <a rel=\"nofollow\" href=\"https://www.reutersagency.com/en/\">Reuters News Agency</a>.</p>\n
 """
         
-        let a = try! XMLDocument(data: .init(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + asd).utf8))
+//        let a = try! XMLDocument(data: .init(("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + asd).utf8))
         
-        debugPrint(a.rootDocument?.children?.first?.children?.first?.stringValue)
-        debugPrint(a.rootDocument?.children?.first?.children?.last?.stringValue)
+        debugPrint("my html <a href=\"\">link text</a>".removingHTML)
+    }
+}
+
+
+extension String {
+    var removingHTML: Self? {
+        let removingDocType = replacingOccurrences(of: "<!DOCTYPE html>", with: "")
+        let wrapped = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>" + removingDocType + "</root>"
+        guard let xml = try? XMLDocument(data: .init(wrapped.utf8)) else { return nil }
+        var children = xml.rootDocument?.children?.first?.children
+        while children?.first?.name?.lowercased() == "html"
+                || children?.first?.name?.lowercased() == "body" {
+            children = children?.first?.children
+        }
+        return children
+            .flatMap {
+                $0.compactMap(\.stringValue)
+            }?
+            .joined(separator: "\n")
     }
 }
