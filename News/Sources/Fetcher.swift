2a9a1f12729cf632ce02c1d7b1a2c20e272415c6
vaux <zero.griffin@gmail.com>
2022-07-22 18:06:34 +0200

    Make parse asynchronous

--- News/Sources/Fetcher.swift
+++ News/Sources/Fetcher.swift
@@ -69,6 +69,7 @@ private extension XMLNode {
             let link = self["link"]?.max8,
             let date = try? Date(pubDate, strategy: strategy)
         else { return nil }
+        
         return (id: guid,
                 item: .init(feed: feed,
                             title: title,
@@ -109,12 +110,13 @@ extension Fetcher {
 }
 
 private final class Parser: NSObject, XMLParserDelegate {
-    private var finished: (() -> Void)?
+    private var finished: (() async -> Void)?
     private var fail: ((Error) -> Void)?
     private var ids = Set<String>()
     private var items = Set<Item>()
     private var item: [String : String]?
     private var element: String?
+    private var completed = [[String : String]]()
     private let synched: Set<String>
     private let strategy: Date.ParseStrategy
     private let feed: Feed
@@ -128,16 +130,102 @@ private final class Parser: NSObject, XMLParserDelegate {
     func parse(data: Data) async throws -> (ids: Set<String>, items: Set<Item>) {
         try await withUnsafeThrowingContinuation { [weak self] continuation in
             let xml = XMLParser(data: data)
-            
             self?.finished = { [weak self] in
                 xml.delegate = nil
                 
-                guard
-                    let ids = self?.ids,
-                    let items = self?.items
-                else { return }
+                guard let self = self else { fatalError() }
+                
+                for item in self.completed {
+                    guard
+                        let guid = item["guid"]?.max8,
+                        !self.synched.contains(guid),
+                        let description = item["description"],
+                        let title = item["title"]?.max8,
+                        let pubDate = item["pubDate"],
+                        let link = item["link"]?.max8,
+                        let date = try? Date(pubDate, strategy: self.strategy)
+                    else { continue }
+                    
+                    self.ids.insert(guid)
+                    self.items.insert(.init(feed: self.feed,
+                                            title: title,
+                                            description: description,
+                                            link: link,
+                                            date: date,
+                                            synched: .now,
+                                            status: .new))
+                }
+                
                 continuation
-                    .resume(returning: (ids, items))
+                    .resume(returning: (self.ids, self.items))
+            }
+            
+            self?.fail = {
+                xml.delegate = self
+                continuation.resume(throwing: $0)
+            }
+            
+            xml.delegate = self
+            xml.parse()
+        }
+    }
+    
+    func parserDidEndDocument(_: XMLParser) {
+        Task {
+            await finished?()
+        }
+    }
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred: Error) {
+        fail?(parseErrorOccurred)
+        parser.abortParsing()
+    }
+    
+    func parser(_ parser: XMLParser, validationErrorOccurred: Error) {
+        fail?(validationErrorOccurred)
+        parser.abortParsing()
+    }
+    
+    func parser(_: XMLParser, didStartElement: String, namespaceURI: String?, qualifiedName: String?, attributes: [String : String] = [:]) {
+        element = ""
+        guard didStartElement == "item" else { return }
+        item = [:]
+    }
+    
+    func parser(_: XMLParser, didEndElement: String, namespaceURI: String?, qualifiedName: String?) {
+        if didEndElement == "item" {
+            if let item = item {
+                completed.append(item)
+                self.item = nil
+            }
+        } else if item != nil, let element = element {
+            item![didEndElement] = element
+            self.element = nil
+        }
+    }
+    
+    func parser(_: XMLParser, foundCharacters: String) {
+        element? += foundCharacters
+    }
+    
+    deinit {
+        print("xml gone")
+    }
+}
+
+private final class Content: NSObject, XMLParserDelegate {
+    private var finished: (() -> Void)?
+    private var fail: ((Error) -> Void)?
+    private var element = ""
+    
+    func parse(data: Data) async throws -> String {
+        try await withUnsafeThrowingContinuation { [weak self] continuation in
+            let xml = XMLParser(data: data)
+            self?.finished = { [weak self] in
+                xml.delegate = nil
+                guard let element = self?.element else { return }
+                continuation
+                    .resume(returning: element)
             }
             
             self?.fail = {
@@ -154,55 +242,30 @@ private final class Parser: NSObject, XMLParserDelegate {
         finished?()
     }
     
-    func parser(_: XMLParser, parseErrorOccurred: Error) {
+    func parser(_ parser: XMLParser, parseErrorOccurred: Error) {
         fail?(parseErrorOccurred)
+        parser.abortParsing()
     }
     
-    func parser(_: XMLParser, validationErrorOccurred: Error) {
+    func parser(_ parser: XMLParser, validationErrorOccurred: Error) {
         fail?(validationErrorOccurred)
+        parser.abortParsing()
     }
     
     func parser(_: XMLParser, didStartElement: String, namespaceURI: String?, qualifiedName: String?, attributes: [String : String] = [:]) {
-        element = ""
-        guard didStartElement == "item" else { return }
-        item = [:]
+
     }
     
     func parser(_: XMLParser, didEndElement: String, namespaceURI: String?, qualifiedName: String?) {
-        if didEndElement == "item" {
-            if let item = item {
-                self.item = nil
-                
-                guard
-                    let guid = item["guid"]?.max8,
-                    !synched.contains(guid),
-                    let description = item["description"],
-                    let title = item["title"]?.max8,
-                    let pubDate = item["pubDate"],
-                    let link = item["link"]?.max8,
-                    let date = try? Date(pubDate, strategy: strategy)
-                else { return }
-                ids.insert(guid)
-                items.insert(.init(feed: feed,
-                                   title: title,
-                                   description: description,
-                                   link: link,
-                                   date: date,
-                                   synched: .now,
-                                   status: .new))
-            }
-        } else if item != nil, let element = element {
-            item![didEndElement] = element
-            self.element = nil
-        }
+        
     }
     
     func parser(_: XMLParser, foundCharacters: String) {
-        element? += foundCharacters
+        element += foundCharacters
     }
     
     deinit {
-        print("gone")
+        print("contnet gone")
     }
 }
 #endif
