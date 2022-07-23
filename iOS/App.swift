5e89f65affd4d397fe988acf6f6c48be89236068
vaux <zero.griffin@gmail.com>
2022-07-23 13:17:25 +0200

    Update navigation

--- iOS/App.swift
+++ iOS/App.swift
@@ -2,15 +2,43 @@ import SwiftUI
 
 @main struct iOSApp: SwiftUI.App {
     @StateObject private var session = Session()
+    @State private var selection = 1
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                Circle()
-                Rectangle()
-                Circle()
+            TabView(selection: $selection) {
+                NavigationView {
+                    Circle()
+                    Rectangle()
+                }
+                .navigationViewStyle(.columns)
+                .tabItem {
+                    Label("Settings", systemImage: "slider.horizontal.3")
+                        .symbolRenderingMode(.hierarchical)
+                }
+                .tag(0)
+                NavigationView {
+                    Circle()
+                    Rectangle()
+                    Circle()
+                }
+                .navigationViewStyle(.columns)
+                .tabItem {
+                    Label("News", systemImage: "newspaper")
+                        .symbolRenderingMode(.hierarchical)
+                }
+                .tag(1)
+                NavigationView {
+                    Circle()
+                    Rectangle()
+                }
+                .navigationViewStyle(.columns)
+                .tabItem {
+                    Label("Recent", systemImage: "clock")
+                        .symbolRenderingMode(.hierarchical)
+                }
+                .tag(2)
             }
-            .navigationViewStyle(.columns)
         }
     }
 }
